1effaf45311d647f559db5855e674148c7dfd85a
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-08-31 10:40:01 -0300

    fix: type statuscode in constructor

--- Promocoes.Application/Promocoes.Application.Input/Receivers/State.cs
+++ Promocoes.Application/Promocoes.Application.Input/Receivers/State.cs
@@ -7,14 +7,14 @@ namespace Promocoes.Application.Input.Receivers
 {
     public class State
     {
-        public State(string statusCode, string message, object data)
+        public State(int statusCode, string message, object data)
         {
             StatusCode = statusCode;
             Message = message;
             Data = data;
         }
 
-        public string StatusCode { get; private set; }
+        public int StatusCode { get; private set; }
         public string Message { get; private set; }
         public object Data { get; private set; }
     }
