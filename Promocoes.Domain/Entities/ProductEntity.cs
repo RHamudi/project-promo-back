ffd8af0e114c28710aa14e7df0e4d37a0c884105
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-08-31 10:32:22 -0300

    fix: constructor properties

--- Promocoes.Domain/Entities/ProductEntity.cs
+++ Promocoes.Domain/Entities/ProductEntity.cs
@@ -9,28 +9,28 @@ namespace Promocoes.Domain.Entities
 {
     public class ProductEntity : BaseEntity, IValidate
     {
-        public ProductEntity( string name, string description, string images, double price,
+        public ProductEntity(string idBusiness ,string name, string description, string images, double price,
             string duration)
         {
             IdProduct = Guid.NewGuid().ToString();
+            IdBusiness = idBusiness;
             Name = name;
             Description = description;
             Images = images;
             Price = price;
-            Duration = duration;
         }
 
         public string IdProduct { get; private set; }
+        public string IdBusiness { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string Images { get; private set; }
         public double Price { get; private set; }
-        public string Duration { get; private set; }
-
 
         public bool IsValid()
         {
-            return new ProductValidations(this).GuidIsValid()
+            return new ProductValidations(this)
+                .GuidIsValid()
                 .NameLength()
                 .DescriptionLength()
                 .IsValid();
