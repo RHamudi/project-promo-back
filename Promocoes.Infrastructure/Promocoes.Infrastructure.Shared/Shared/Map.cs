b9cf2b2e2b8a3ce80982cbe1e226ab6e4389c8aa
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-08-31 10:53:24 -0300

    feat: create methods for table

---
+++ Promocoes.Infrastructure/Promocoes.Infrastructure.Shared/Shared/Map.cs
@@ -0,0 +1,20 @@
+namespace Promocoes.Infrastructure.Shared.Shared
+{
+    public class Map
+    {
+        public static string GetTableBusiness()
+        {
+            return "[tb_business]";
+        }
+
+        public static string GetTableProduct()
+        {
+            return "[tb_product]";
+        }
+
+        public static string GetTablePromotion()
+        {
+            return "[tb_promotion]";
+        }
+    }
+}
