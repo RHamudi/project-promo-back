883d13cea1235217c77332c77e44bc937bad77c7
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-08-31 11:00:50 -0300

    feat: adding logic route insert business

--- Promocoes.Application/Promocoes.Application.Input/Receivers/BusinessReceiver/Insert.cs
+++ Promocoes.Application/Promocoes.Application.Input/Receivers/BusinessReceiver/Insert.cs
@@ -1,13 +1,39 @@
+using System.Reflection.Metadata.Ecma335;
+using MediatR;
 using Promocoes.Application.Input.Commands.BusinessContext;
-using Promocoes.Application.Input.Receivers.Interfaces;
+using Promocoes.Application.Input.Repositories.Interfaces;
+using Promocoes.Domain.Entities;
 
 namespace Promocoes.Application.Input.Receivers.BusinessReceiver
 {
-    public class Insert : IReceiver<BusinessCommand, State>
+    public class Insert : IRequestHandler<BusinessCommand, State>
     {
-        public State Action(BusinessCommand command)
+        private readonly IWriteBusinessRepository _repository;
+
+        public Insert(IWriteBusinessRepository repository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+        }
+
+        public Task<State> Handle(BusinessCommand request, CancellationToken cancellationToken)
+        {
+
+            var business = new BusinessEntity(request.Name, request.Password, request.Description,
+                request.Logo, request.Location, request.Contact, request.Category,
+                request.Operation, request.GeoData, request.IsAdmin);
+
+            if(!business.IsValid())
+                return Task.FromResult(new State(400, "Erro ao inserir Empresa", business.Notifications));
+            
+            try 
+            {
+                _repository.InsertBusiness(business);
+                return Task.FromResult(new State(200, "Empresa adicionada com sucesso", business));
+            }
+            catch(Exception ex)
+            {
+                return Task.FromResult(new State(500, ex.Message, null));
+            }
         }
     }
 }
