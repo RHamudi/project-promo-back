e498cc4bcf88acef38766b1aa313a20b1b6b209a
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-09-04 10:51:14 -0300

    feat: adding constructor class

--- Promocoes.Application/Promocoes.Application.Input/Commands/BusinessContext/BusinessCommand.cs
+++ Promocoes.Application/Promocoes.Application.Input/Commands/BusinessContext/BusinessCommand.cs
@@ -7,6 +7,20 @@ namespace Promocoes.Application.Input.Commands.BusinessContext
 {
     public class BusinessCommand : IRequest<State>
     {
+        public BusinessCommand(string name, string password, string description, string logo, string location, Contacts contact, ECategory category, string operation, string geoData, bool isAdmin)
+        {
+            Name = name;
+            Password = password;
+            Description = description;
+            Logo = logo;
+            Location = location;
+            Contact = contact;
+            Category = category;
+            Operation = operation;
+            GeoData = geoData;
+            IsAdmin = isAdmin;
+        }
+
         public string Name { get; private set; }
         public string Password { get; private set; }
         public string Description { get; private set; }
