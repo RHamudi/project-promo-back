039fc0172b8e4f72eb2353c997c4163c79cfc787
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-09-04 10:45:58 -0300

    feat: return class

---
+++ Promocoes.Application/Promocoes.Application.Output/State.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promocoes.Application.Output
+{
+    public class State
+    {
+        public State(int statusCode, string message, object data)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+        }
+
+        public int StatusCode { get; private set; }
+        public string Message { get; private set; }
+        public object Data { get; private set; }
+    }
+}
