78393b15df2e33d10427b73e4ce15c1ee19cef8b
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-09-22 11:15:56 -0300

    feat: create query sql

--- Promocoes.Infrastructure/Promocoes.Infrastructure.Output/Queries/ProductQueries.cs
+++ Promocoes.Infrastructure/Promocoes.Infrastructure.Output/Queries/ProductQueries.cs
@@ -27,5 +27,28 @@ namespace Promocoes.Infrastructure.Output.Queries
 
             return new QueryModel(this.Query, null);
         }
+
+        public QueryModel GetProductsByIdBusiness(Guid idEmpresa)
+        {
+            this.Table = Map.GetTableProduct();
+
+            this.Query = @$"
+            SELECT
+                tp.IdProduct as IdProduto,
+                tp.IdBusiness as IdEmpresa,
+                tp.Name as Nome,
+                tp.Description as Descricao,
+                tp.Images as Imagens,
+                tp.Price as Price
+            FROM 
+                {this.Table} tp WHERE IdBusiness = @id
+            ";
+
+            this.Parameters = new {
+                id = idEmpresa
+            };
+
+            return new QueryModel(this.Query, this.Parameters);
+        }
     }
 }
