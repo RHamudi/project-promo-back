64eacd785c937e0bdb9fe3131306ecafb14aa4dd
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-09-27 11:11:12 -0300

    fix: indentation

--- Promocoes.Application/Promocoes.Application.Output/Interfaces/IReadBusinessRepository.cs
+++ Promocoes.Application/Promocoes.Application.Output/Interfaces/IReadBusinessRepository.cs
@@ -5,7 +5,6 @@ namespace Promocoes.Application.Output.Interfaces
     public interface IReadBusinessRepository
     {
         IEnumerable<AllBusinessDTO> GetAllBusiness();
-        
         AllBusinessDTO GetBusinessById(Guid idEmpresa);
     }
 }
