a27b07d6e0e0c84d2a4717d63fdf39e9e9ba5eb8
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-09-27 11:04:00 -0300

    fix: remove constructor

--- Promocoes.Application/Promocoes.Application.Input/Commands/ProductContext/ProductCommand.cs
+++ Promocoes.Application/Promocoes.Application.Input/Commands/ProductContext/ProductCommand.cs
@@ -1,24 +1,16 @@
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Promocoes.Application.Input.Receivers;
 
 namespace Promocoes.Application.Input.Commands.ProductContext
 {
     public class ProductCommand : IRequest<State>
     {
-        public ProductCommand(string idBusiness, string name, string description, string images, double price)
-        {
-            IdBusiness = idBusiness;
-            Name = name;
-            Description = description;
-            Images = images;
-            Price = price;
-        }
-
-        public string IdBusiness { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public string Images { get; private set; }
-        public double Price { get; private set; }
+        public string IdBusiness { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Images { get; set; }
+        public double Price { get; set; }
 
     }
 }
