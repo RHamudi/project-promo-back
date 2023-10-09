4d71f126373b92e321b6c6f873dbab89993e5591
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-09 10:00:42 -0300

    repo

--- Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IWriteProductRepository.cs
+++ Promocoes.Application/Promocoes.Application.Input/Repositories/Interfaces/IWriteProductRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Promocoes.Domain.Entities;
 
 namespace Promocoes.Application.Input.Repositories.Interfaces
