f7c9218d232d01dd0d1b71dca9172925ab31fc9a
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-09 09:22:58 -0300

    feat: testes jwt create/validation

---
+++ Promocoes.Tests/Tests/JwtTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using Promocoes.Application.Input.Commands.BusinessContext;
+using Promocoes.Application.Input.Services.Jwt;
+
+namespace Promocoes.Tests.Tests
+{
+    [TestClass]
+    public class JwtTests
+    {
+        [TestMethod]
+        public void Criar_Token_Jwt()
+        {
+            var model = new AuthenticationCommand("123456", "ramonramos.silva19@gmail.com");
+            var jwToken = new CreateToken(model);
+            Assert.IsNotNull(jwToken.Token);
+        }
+
+        [TestMethod]
+        public void Verificar_Jwt_valido()
+        {
+            var model = new AuthenticationCommand("123456", "ramonramos.silva19@gmail.com");
+            var jwToken = new CreateToken(model);
+            var validateToken = ValidateToken.VerifyToken(jwToken.Token);
+
+            Assert.AreEqual(true, validateToken);
+        }
+    }
+}
