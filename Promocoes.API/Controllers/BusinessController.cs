d247a7ee6037e4de8934ba4e5496547478127a4d
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-10-25 09:07:42 -0300

    feat: adding no cache in route auth

--- Promocoes.API/Controllers/BusinessController.cs
+++ Promocoes.API/Controllers/BusinessController.cs
@@ -44,10 +44,11 @@ namespace Promocoes.API.Controllers
             return Ok(result);
         }
 
+        [OutputCache(NoStore = true, Duration = 0)]
         [HttpPost("Authentication")]
         public async Task<IActionResult> Authentication([FromBody] AuthenticationCommand model)
         {
-            var result = await _mediator.Send(new AuthenticationCommand(model.Email, model.Password));
+            var result = await _mediator.Send(model);
             return Ok(result);
         }
     }
