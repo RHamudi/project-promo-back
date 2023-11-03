48841fa5d11e3f086178b75fc28b884390f24a53
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-03 12:15:27 -0300

    feat: create user entity

---
+++ Promocoes.Domain/Entities/UserEntity.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Promocoes.Domain.Validations;
+using Promocoes.Domain.Validations.Interfaces;
+
+namespace Promocoes.Domain.Entities
+{
+    public class UserEntity : BaseEntity, IValidate
+    {
+        public UserEntity(string name, string email, string password, string idBusiness)
+        {
+            IdUser = Guid.NewGuid().ToString();
+            Name = name;
+            Email = email;
+            Password = password;
+            IdBusiness = idBusiness;
+        }
+
+        public string IdUser { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string IdBusiness { get; set; }
+
+        public bool IsValid()
+        {
+            return new UserValidations(this)
+                .GuidIsValid()
+                .NameIsValid()
+                .EmailIsValid()
+                .NameLength()
+                .PasswordLength()
+                .IsValid();
+        }
+    }
+}
