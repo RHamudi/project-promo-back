95f466e21bc364a7d6bb5ae9ca445d90dff390e1
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-06 07:37:26 -0300

    feat: running insert query in db

---
+++ Promocoes.Infrastructure/Promocoes.Infrastructure.Input/Repositories/WriteUserRepository.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Promocoes.Application.Input.Repositories.Interfaces;
+using Promocoes.Domain.Entities;
+using Promocoes.Infrastructure.Input.Queries;
+using Promocoes.Infrastructure.Shared.Factory;
+
+namespace Promocoes.Infrastructure.Input.Repositories
+{
+    public class WriteUserRepository : IWriteUserRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public WriteUserRepository(IDbConnection connection)
+        {
+            _connection = SqlFactory.SqlFactoryConnection();
+        }
+
+        public void InsertUser(UserEntity user)
+        {
+            var query = new UserQueries().InsertUser(user);
+
+            try
+            {
+                using(_connection)
+                {
+                    _connection.Query(query.Query, query.Parameters);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
