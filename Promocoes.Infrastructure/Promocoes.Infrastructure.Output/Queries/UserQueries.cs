1b653086bc1639669fb43613451c0322592c4831
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-09 15:12:52 -0300

    feat: create sql query user by id

--- Promocoes.Infrastructure/Promocoes.Infrastructure.Output/Queries/UserQueries.cs
+++ Promocoes.Infrastructure/Promocoes.Infrastructure.Output/Queries/UserQueries.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Promocoes.Application.Output.DTOs;
 using Promocoes.Infrastructure.Shared.Shared;
 
 namespace Promocoes.Infrastructure.Output.Queries
@@ -25,5 +26,28 @@ namespace Promocoes.Infrastructure.Output.Queries
             
             return new QueryModel(this.Query, null);
         }
+
+        public QueryModel GetUserById(UserDTO command)
+        {
+            this.Table = Map.GetTableUser();
+
+            this.Query = $@"
+                        SELECT
+                            tb.Name,
+                            tb.Email,
+                            tb.Password,
+                            tb.IdBusiness 
+                        FROM
+                            tb_users tb WHERE IdUser = @IdUser
+                        ";
+            
+            this.Parameters = new {
+                IdUser = command.IdUsuario
+            };
+
+            return new QueryModel(this.Query, this.Parameters);
+        }
     }
+
+    
 }
