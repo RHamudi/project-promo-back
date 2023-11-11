e0d8e966972fb918165e3fefce495758fb426dbe
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-10 21:58:09 -0300

    feat: adding prop

--- Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/UpdateUserCommand.cs
+++ Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/UpdateUserCommand.cs
@@ -13,5 +13,6 @@ namespace Promocoes.Application.Input.Commands.UserContext
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public Guid? IdEmpresa { get; set; }
     }
 }
