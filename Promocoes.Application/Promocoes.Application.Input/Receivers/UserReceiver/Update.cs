67f4f75701000220835af13bdc084d56ddd0e193
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-11-22 12:50:14 -0300

    feat: adding new repository

--- Promocoes.Application/Promocoes.Application.Input/Receivers/UserReceiver/Update.cs
+++ Promocoes.Application/Promocoes.Application.Input/Receivers/UserReceiver/Update.cs
@@ -7,6 +7,7 @@ using MediatR;
 using Promocoes.Application.Input.Commands.UserContext;
 using Promocoes.Application.Input.Repositories.Interfaces;
 using Promocoes.Application.Output.DTOs;
+using Promocoes.Application.Output.Interfaces;
 using Promocoes.Domain.Entities;
 
 namespace Promocoes.Application.Input.Receivers.UserReceiver
@@ -14,27 +15,30 @@ namespace Promocoes.Application.Input.Receivers.UserReceiver
     public class Update : IRequestHandler<UpdateUserCommand, State>
     {
         private readonly IWriteUserRepository _repository;
+        private readonly IReadUserRepository _readRepository;
         private readonly IMapper _mapper;
 
-        public Update(IWriteUserRepository repository, IMapper mapper)
+        public Update(IWriteUserRepository repository, IReadUserRepository readRepository, IMapper mapper)
         {
+            _readRepository = readRepository;
             _repository = repository;
             _mapper = mapper;
         }
 
         public Task<State> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
-            var userById = _repository.GetUserById(request.IdUser);
+            var userById = _readRepository.GetUserById(request.IdUser);
 
             if(userById == null)
                 return Task.FromResult(new State(400, "Usuario não encontrado", request.IdUser));
             
+            
             UpdateUserCommand userUpdated = new() {
                 IdUser = request.IdUser,
-                Name = request.Name != userById.Name & request.Name != null ? request.Name :  userById.Name, 
+                Name = request.Name != userById.Nome & request.Name != null ? request.Name :  userById.Nome, 
                 Email = request.Email != userById.Email & request.Email != null ? request.Email : userById.Email,
-                Password = request.Password != userById.Password  & request.Password != null ? request.Password : userById.Password,
-                IdBusiness = request.IdBusiness != userById.IdBusiness & request.IdBusiness != null ? request.IdBusiness : userById.IdBusiness 
+                Password = request.Password != userById.Senha  & request.Password != null ? request.Password : userById.Senha,
+                IdBusiness = request.IdBusiness != userById.IdEmpresa & request.IdBusiness != null ? request.IdBusiness : userById.IdEmpresa 
             };
 
             try
