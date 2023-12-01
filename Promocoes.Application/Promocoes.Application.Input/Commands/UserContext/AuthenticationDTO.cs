1c73102599bb78156453c30dcef7e184b0d40075
RHamudi <94305379+RHamudi@users.noreply.github.com>
2023-12-01 16:11:50 -0300

    feat: creating return authentication dto

---
+++ Promocoes.Application/Promocoes.Application.Input/Commands/UserContext/AuthenticationDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Promocoes.Application.Input.Commands.UserContext
+{
+    public class AuthenticationDTO
+    {
+         public Guid IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Guid? IdEmpresa { get; set; }
+    }
+}
