2a01520d76957b29db0daf811861fe3dcf53e303
RHamudi <94305379+RHamudi@users.noreply.github.com>
2024-01-11 10:26:25 -0300

    Update ClientSmtp
    Update SendEmail method in ClientSmtp class to include token in email message.

--- Promocoes.Application/Promocoes.Application.Input/Services/SendEmail/ClientSmtp.cs
+++ Promocoes.Application/Promocoes.Application.Input/Services/SendEmail/ClientSmtp.cs
@@ -9,11 +9,14 @@ namespace Promocoes.Application.Input.Services.SendEmail
 {
     public class ClientSmtp
     {
-        public static Task SendEmail(string email, string subject, string message)
+        public static Task SendEmail(string email, string subject, string token)
         {
             var mail = "ramonramos.silva@outlook.com";
             var pw = "ramonramos.";
 
+            var message = @$"Por favor Não compartilhe este link com ninguem
+                            Clique aqui para verificar sua conta: http://192.168.1.67:5293/api/user/VerifyUser?Token={token}
+            ";
             
             var client = new SmtpClient("smtp-mail.outlook.com", 587)
             {
