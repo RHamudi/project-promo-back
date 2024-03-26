d9957cf8843aa04cae3c7401691ecc1ad6a65927
Hamudi <ramonramos.silva16@gmail.com>
2024-03-26 15:31:50 -0300

    test

--- Promocoes.API/Program.cs
+++ Promocoes.API/Program.cs
@@ -24,7 +24,7 @@ builder.WebHost.ConfigureKestrel((context, serverOptions) =>
     serverOptions.Configure(kestrelSection)
         .Endpoint("Http", listenOptions =>
         {
-            // ...
+            
         });
 });
 
