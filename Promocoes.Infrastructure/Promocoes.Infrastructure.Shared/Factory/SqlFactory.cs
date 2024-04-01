6ce95ba1ca97006fa05be40571c137fdf6dbd493
Hamudi <ramonramos.silva16@gmail.com>
2024-04-01 11:42:06 -0300

    feat: new config using .env file

--- Promocoes.Infrastructure/Promocoes.Infrastructure.Shared/Factory/SqlFactory.cs
+++ Promocoes.Infrastructure/Promocoes.Infrastructure.Shared/Factory/SqlFactory.cs
@@ -7,10 +7,14 @@ namespace Promocoes.Infrastructure.Shared.Factory
 {
     public class SqlFactory
     {
+
         public static IDbConnection SqlFactoryConnection()
-        {   //NpgsqlConnection("Server=project-promo-11154.6wr.aws-us-west-2.cockroachlabs.cloud;Port=26257;Database=defaultdb;Username=project-promo;Password=TqkXkqn_G9OAEz5u0aITkQ;");
-            // SqlConnection("Server=localhost;Database=promo_database;Trusted_Connection=True;TrustServerCertificate=True;");
-            return new NpgsqlConnection("Server=project-promo-11154.6wr.aws-us-west-2.cockroachlabs.cloud;Port=26257;Database=defaultdb;Username=project-promo;Password=TqkXkqn_G9OAEz5u0aITkQ;");
+        {
+            DotNetEnv.Env.Load();
+            DotNetEnv.Env.TraversePath().Load();
+  
+            var apiKey = Environment.GetEnvironmentVariable("SQL_LOCALHOST");
+            return new SqlConnection(apiKey);
         }
     }
 }
