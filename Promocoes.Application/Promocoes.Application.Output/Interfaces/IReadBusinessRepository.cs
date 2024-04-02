e9d3eb801cdb2112beaf051ace32c3afbe8c6d24
Hamudi <ramonramos.silva16@gmail.com>
2024-04-02 16:52:34 -0300

    feat: modify return in methods

--- Promocoes.Application/Promocoes.Application.Output/Interfaces/IReadBusinessRepository.cs
+++ Promocoes.Application/Promocoes.Application.Output/Interfaces/IReadBusinessRepository.cs
@@ -1,10 +1,11 @@
+using ErrorOr;
 using Promocoes.Application.Output.DTOs;
 
 namespace Promocoes.Application.Output.Interfaces
 {
     public interface IReadBusinessRepository
     {
-        IEnumerable<AllBusinessDTO> GetAllBusiness();
-        AllBusinessDTO GetBusinessById(Guid idEmpresa);
+        ErrorOr<IEnumerable<AllBusinessDTO>> GetAllBusiness();
+        ErrorOr<AllBusinessDTO> GetBusinessById(Guid idEmpresa);
     }
 }
